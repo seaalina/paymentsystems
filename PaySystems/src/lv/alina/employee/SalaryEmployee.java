1c20dc1bd7073c6120b8eed1c00fdcbd78043b77
seaalina <rogacheva.alina@gmail.com>
2020-02-14 20:20:17 +0200

    add salary employee

---
+++ PaySystems/src/lv/alina/employee/SalaryEmployee.java
@@ -0,0 +1,38 @@
+package lv.alina.employee;
+
+import lv.alina.paysystem.Payee;
+
+public class SalaryEmployee implements Payee {
+	
+	private String name;
+	private Integer bankAccount;
+	protected Double grossWage;
+	
+	
+
+	public SalaryEmployee(String name, Integer bankAccount, Double grossWage) {
+		
+		this.name = name;
+		this.bankAccount = bankAccount;
+		this.grossWage = grossWage;
+	}
+
+	@Override
+	public String name() {
+	
+		return name;
+	}
+
+	@Override
+	public Double grossPayment() {
+		return grossWage;
+	}
+
+	@Override
+	public Integer bankAccount() {
+		return bankAccount;
+	}
+
+	
+	
+}
